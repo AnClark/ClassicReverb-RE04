83b660c5a726e5bcb11e61b11804c0bd53267bba
AnClark <anclarkliu@outlook.com>
2026-03-10 22:52:15 +0800

    Rename plugin brand & name
    Directly writing Kjaerhus Audio in vendor name may have legal issues, and may confuse end users.

--- src/DistrhoPluginInfo.h
+++ src/DistrhoPluginInfo.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define DISTRHO_PLUGIN_NAME    "Classic Reverb RE-04"
-#define DISTRHO_PLUGIN_URI     "urn:classic-reverb:1"
-#define DISTRHO_PLUGIN_BRAND   "Kjaerhus Audio, AnClark Liu"
+#define DISTRHO_PLUGIN_NAME    "Classic Reverb RE-04 | Kjaerhus Classic Reverb Clone"
+#define DISTRHO_PLUGIN_URI     "https://github.com/AnClark/ClassicReverb-RE04"
+#define DISTRHO_PLUGIN_BRAND   "AnClark Liu"
 #define DISTRHO_PLUGIN_CLAP_ID "studio.anclark.classic.reverb.re04"
 
 #define DISTRHO_PLUGIN_NUM_INPUTS   2
