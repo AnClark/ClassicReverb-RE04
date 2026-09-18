93a9469b9af834b96db7cb9ba00e79e99c9aadb3
AnClark Liu <clarklaw4701@qq.com>
2026-09-18 21:28:04 +0800

    fix(build): Improve Win32 cross-build logic for macOS
    - Use dynamic paths for compilers in Makefile
    - Explicitly specify Temp directory on macOS (for build directory)

--- Makefile.win32_cross.mk
+++ Makefile.win32_cross.mk
@@ -27,6 +27,7 @@ ifeq ($(UNAME_S),Linux)
   TMPDIR := /tmp
 else ifeq ($(UNAME_S),Darwin)
   $(info Detected operating system: macOS)
+  TMPDIR := /tmp
 else ifeq ($(UNAME_O),Msys)
   $(info Detected operating system: Windows (Msys2))
 	$(error Detected Msys2 environment. Run this pipeline from Linux or macOS instead.)
@@ -39,7 +40,7 @@ endif
 all: package
 
 check_dependencies:
-	@which x86_64-w64-mingw32-g++ > /dev/null || (echo "Error: x86_64-w64-mingw32-g++ is not installed." && exit 1)
+	@which x86_64-w64-mingw32-g++ > /dev/null || (echo "Error: x86_64-w64-mingw32-g++ is not installed. Please install MinGW-w64." && exit 1)
 	@which cmake > /dev/null || (echo "Error: cmake is not installed." && exit 1)
 	@which ninja > /dev/null || (echo "Error: ninja is not installed." && exit 1)
 	@which ccache > /dev/null || (echo "Error: ccache is not installed." && exit 1)
@@ -51,10 +52,10 @@ configure: check_dependencies
 	@mkdir -p $(BUILD_DIR)
 	@echo "SET(CMAKE_SYSTEM_NAME Windows)" > $(WIN32_TOOLCHAIN_FILE)
 	@echo "SET(CMAKE_SYSTEM_PROCESSOR x86_64)" >> $(WIN32_TOOLCHAIN_FILE)
-	@echo "SET(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)" >> $(WIN32_TOOLCHAIN_FILE)
-	@echo "SET(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)" >> $(WIN32_TOOLCHAIN_FILE)
-	@echo "SET(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)" >> $(WIN32_TOOLCHAIN_FILE)
-	@echo "SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32/ ${PREFIX_PATH})" >> $(WIN32_TOOLCHAIN_FILE)
+	@echo "SET(CMAKE_C_COMPILER $(shell which x86_64-w64-mingw32-gcc))" >> $(WIN32_TOOLCHAIN_FILE)
+	@echo "SET(CMAKE_CXX_COMPILER $(shell which x86_64-w64-mingw32-g++))" >> $(WIN32_TOOLCHAIN_FILE)
+	@echo "SET(CMAKE_RC_COMPILER $(shell which x86_64-w64-mingw32-windres))" >> $(WIN32_TOOLCHAIN_FILE)
+	@echo "SET(CMAKE_FIND_ROOT_PATH $(shell x86_64-w64-mingw32-gcc -print-sysroot) ${PREFIX_PATH})" >> $(WIN32_TOOLCHAIN_FILE)
 
 	@cmake -S . -B $(BUILD_DIR) -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache \
 		-DCMAKE_TOOLCHAIN_FILE=$(WIN32_TOOLCHAIN_FILE)
